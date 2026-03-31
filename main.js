ea4ada40a2fa9b1273eb497846ad69c1f9e3d211
SDSOFIK <sdsofik2002@gmail.com>
2026-03-31 18:05:37 +0600

    js instruction and setup js all commant main.js file

---
+++ main.js
@@ -0,0 +1,10 @@
+// what is a java secript ? and why used 
+/* java secript is a Programming language of web এক কথায় Java secript web site ডাইনামিক ও ইন্টার‍্যাকটিভ করে তোলে 
+
+java sceript মুল একটা ওয়েব সাইট কে জিবন্ত করে তুলে যেমন button click করেলে নতুন সাইট ওয়েব ওপেন হয়ে   data update date change animation এইগুলা কাজ করে থাকে */
+
+// java secript setup and run 
+/* java secript file name .js like main.js index.js অথাবা আমরা <secript></secript> tag লিখেতে পারি html file . javesecript links html file a body section a sebr ses a <secript src= " Js file addres"> </secript> java secript run kore output 3 vabe dakhe pari 1. web a giye inspect kore console a output dakhte pay and 2. node js install থাকতে হবে এবং Terminal a commde dibe (node file name  ) 
+*/
+
+// console.log("hello js")
