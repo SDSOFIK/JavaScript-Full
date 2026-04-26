a72621d24acf3fc984234ef3025af86ba5c79aa1
SDSOFIK <sdsofik2002@gmail.com>
2026-04-26 16:48:28 +0600

    add oop

---
+++ oop.js
@@ -0,0 +1,26 @@
+//============== oop.js ==============
+// Object Oriented Programming in JavaScript
+//what is OOP?
+//OOP is a programming paradigm that uses objects and classes to organize code and data. It allows developers to create reusable and modular code by encapsulating data and behavior into objects.
+// oop 4 টি মূল ধারণা আছে:
+//1. Encapsulation: 
+//2. Inheritance: 
+//3. Polymorphism: 
+//4. Abstraction: 
+
+// ============================ class ============================
+
+// class হল এমন একটা design and tempelet যেখানে আমরা মেথড বা সব কিছু design করে রাখে দেই পরে object Create kore acess kori ক্লাস এর সকল probaty 
+// class keyword diye class শুরু করতে হয় ক্লাস নামে varible নামে মত নিয়েম 
+class addClas{
+name = "Sofik"
+age = 20;
+
+};
+
+// object crate kno akta virible মাঝে রাখে new keyword diye class name , 
+let a = new addClas()
+// ক্লাস এর মাঝে থেকে আমরা যে propty ta চাই ওইটা নিব 
+console.log(a.name , a.age);
+// =========================== encapsulation ============== 
+// encapsulation হল এমন একটা 
